3bfb8f4f098123d266f002cffc9913738c0a741b
Naoya Inoue <0929naoya@gmail.com>
2019-08-16 20:17:22 +0900

    change api -> coinmarketcap pro api
    need API_KEY environment

--- coinmarketcap.py
+++ coinmarketcap.py
@@ -7,9 +7,13 @@ import logging
 import requests
 import sys
 import time
+import os
 
 # lock of the collect method
 lock = Lock()
+
+# coinmarketcap pro api key
+CMC_PRO_API_KEY = os.environ['API_KEY']
 
 # caching API for 2min
 cache = TTLCache(maxsize=1000, ttl=120)
@@ -25,56 +29,55 @@ log.addHandler(ch)
 
 
 class CoinClient():
-	def __init__(self):
-		self.endpoint = 'https://api.coinmarketcap.com/v2/ticker/'
+    def __init__(self):
+        self.endpoint = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest'
 
-	@cached(cache)
-	def tickers(self, start):
-		r = requests.get('%s?convert=BTC&start=%d' % (self.endpoint, start))
-		return json.loads(r.content.decode('UTF-8'))
+    @cached(cache)
+    def tickers(self):
+        r = requests.get('%s?id=1,1027,2797&CMC_PRO_API_KEY=%s' % (self.endpoint, CMC_PRO_API_KEY))
+        return json.loads(r.content.decode('UTF-8'))
 
 class CoinCollector():
-	def __init__(self):
-		self.client = CoinClient()
+    def __init__(self):
+        self.client = CoinClient()
 
-	def collect(self):
-		with lock:
-			log.info('collecting...')
-			start = 1
-			while True:
-				# query the api
-				response = self.client.tickers(start)
-				if not response['data']:
-					break
-				metric = Metric('coin_market', 'coinmarketcap metric values', 'gauge')
-				for _, value in response['data'].items():
-					for that in ['rank', 'total_supply', 'max_supply', 'circulating_supply']:
-						coinmarketmetric = '_'.join(['coin_market', that])
-						if value[that] is not None:
-							metric.add_sample(coinmarketmetric, value=float(value[that]), labels={'id': value['website_slug'], 'name': value['name'], 'symbol': value['symbol']})
-					for price in ['USD', 'BTC']:
-						for that in ['price', 'volume_24h', 'market_cap', 'percent_change_1h', 'percent_change_24h', 'percent_change_7d']:
-							coinmarketmetric = '_'.join(['coin_market', that, price]).lower()
-							if value['quotes'][price] is None:
-								continue
-							if value['quotes'][price][that] is not None:
-								metric.add_sample(coinmarketmetric, value=float(value['quotes'][price][that]), labels={'id': value['website_slug'], 'name': value['name'], 'symbol': value['symbol']})
-				yield metric
-				start += len(response['data'])
+    def collect(self):
+        with lock:
+            log.info('collecting...')
+            #start = 1
+            # query the api
+            response = self.client.tickers()
+            if not response['data']:
+                log.info('HTTP error')
+            metric = Metric('coin_market', 'coinmarketcap metric values', 'gauge')
+            for _, value in response['data'].items():
+                for that in ['cmc_rank', 'total_supply', 'max_supply', 'circulating_supply']:
+                    coinmarketmetric = '_'.join(['coin_market', that])
+                    if value[that] is not None:
+                        metric.add_sample(coinmarketmetric, value=float(value[that]), labels={'id': value['slug'], 'name': value['name'], 'symbol': value['symbol']})
+                for price in ['USD']:
+                    for that in ['price', 'volume_24h', 'market_cap', 'percent_change_1h', 'percent_change_24h', 'percent_change_7d']:
+                        coinmarketmetric = '_'.join(['coin_market', that, price]).lower()
+                        if value['quote'][price] is None:
+                            continue
+                        if value['quote'][price][that] is not None:
+                            metric.add_sample(coinmarketmetric, value=float(value['quote'][price][that]), labels={'id': value['slug'], 'name': value['name'], 'symbol': value['symbol']})
+            yield metric
+            #start += len(response['data'])
 
 if __name__ == '__main__':
-	try:
-		parser = argparse.ArgumentParser(description=__doc__, formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-		parser.add_argument('--port', nargs='?', const=9101, help='The TCP port to listen on', default=9101)
-		parser.add_argument('--addr', nargs='?', const='0.0.0.0', help='The interface to bind to', default='0.0.0.0')
-		args = parser.parse_args()
-		log.info('listening on http://%s:%d/metrics' % (args.addr, args.port))
+    try:
+        parser = argparse.ArgumentParser(description=__doc__, formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+        parser.add_argument('--port', nargs='?', const=9101, help='The TCP port to listen on', default=9101)
+        parser.add_argument('--addr', nargs='?', const='0.0.0.0', help='The interface to bind to', default='0.0.0.0')
+        args = parser.parse_args()
+        log.info('listening on http://%s:%d/metrics' % (args.addr, args.port))
 
-		REGISTRY.register(CoinCollector())
-		start_http_server(int(args.port), addr=args.addr)
+        REGISTRY.register(CoinCollector())
+        start_http_server(int(args.port), addr=args.addr)
 
-		while True:
-			time.sleep(60)
-	except KeyboardInterrupt:
-		print(" Interrupted")
-		exit(0)
+        while True:
+            time.sleep(60)
+    except KeyboardInterrupt:
+        print(" Interrupted")
+        exit(0)
